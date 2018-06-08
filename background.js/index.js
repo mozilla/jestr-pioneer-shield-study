fdbeaed8d6c7c2ebaedbdf2b5ec5662df846187a
Fredrik Wollsén <fredrik@neam.se>
2018-06-08 12:25:10 +0300

    Removed unused startup and install event handlers

--- background.js/index.js
+++ background.js/index.js
@@ -27,35 +27,6 @@ async function runOnce() {
   startStoringJsInstrumentationResultsInDb(variation);
 }
 
-/**
- * Fired when a profile that has this extension installed first starts up.
- * This event is not fired when a private browsing/incognito profile is started.
- */
-function handleStartup() {
-  console.log("handleStartup", arguments);
-}
-
-browser.runtime.onStartup.addListener(handleStartup);
-
-/**
- * Fired when the extension is first installed, when the extension is updated
- * to a new version, and when the browser is updated to a new version.
- * @param details
- */
-function handleInstalled(details) {
-  console.log("handleInstalled", details.reason, details);
-}
-
-browser.runtime.onInstalled.addListener(handleInstalled);
-
-// todo: on shutdown
-// Run shutdown-related non-privileged code
-// Remove db - deleteDb
-
-// actually start
-runOnce();
-
-
 // For devtools messages
 
 /**
