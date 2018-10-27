0b2dbf51e20f897f7c22feda4dd50ddaeca1649c
Fredrik Wollsén <fredrik@neam.se>
2018-10-28 02:36:22 +0300

    Removed stub code for devtools-panel

--- feature.js/index.js
+++ feature.js/index.js
@@ -27,27 +27,6 @@ class Feature {
     await initDb();
     await storeInDb("studyLog", { variation: variation.name, event: "run" });
 
-    // For devtools messages
-
-    /**
-     When we receive the message, execute the given script in the given
-     tab.
-     */
-    function handleMessage(request, sender) {
-      if (sender.url !== browser.runtime.getURL("/devtools/panel/panel.html")) {
-        return;
-      }
-
-      browser.tabs.executeScript(request.tabId, {
-        code: request.script,
-      });
-    }
-
-    /**
-     Listen for messages from our devtools panel.
-     */
-    browser.runtime.onMessage.addListener(handleMessage);
-
     // perform something only during first run
     if (isFirstRun) {
       await storeInDb("studyLog", {
