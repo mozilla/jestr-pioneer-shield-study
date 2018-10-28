a8a12c741e91ae218dc3860fd7e7a2bad59b3833
Fredrik Wollsén <fredrik@neam.se>
2018-10-28 12:41:41 +0200

    Convert all telemetry payload attributes to strings before submitting

--- feature.js/telemetrySender.js
+++ feature.js/telemetrySender.js
@@ -1,7 +1,29 @@
 export class TelemetrySender {
   async submitTelemetryPayload(interfaceName, payload) {
-    const stringStringMap = { ...payload };
+    const stringStringMap = this.createShieldPingPayload(payload);
     return this.sendTelemetry(stringStringMap);
+  }
+
+  // TODO: @glind: move to shield study utils?
+  createShieldPingPayload(shieldPingAttributes) {
+    const shieldPingPayload = {};
+
+    // shield ping attributes must be strings
+    for (const attribute in shieldPingAttributes) {
+      let attributeValue = shieldPingAttributes[attribute];
+      if (typeof attributeValue === "undefined") {
+        attributeValue = "null";
+      }
+      if (typeof attributeValue === "object") {
+        attributeValue = JSON.stringify(attributeValue);
+      }
+      if (typeof attributeValue !== "string") {
+        attributeValue = String(attributeValue);
+      }
+      shieldPingPayload[attribute] = attributeValue;
+    }
+
+    return shieldPingPayload;
   }
 
   async sendTelemetry(stringStringMap) {
