c07390f0a1640ec83271438c1f7ca0272f8834a0
Fredrik Wollsén <fredrik@neam.se>
2018-10-28 12:41:06 +0200

    Logging tweaks

--- feature.js/dataReceiver.js
+++ feature.js/dataReceiver.js
@@ -10,42 +10,42 @@ export const logInfo = async function(msg) {
 export const logDebug = async function(msg) {
   const level = "debug";
   const logEntry = { level, msg };
-  await browser.study.logger.debug(["dataReceiver", logEntry]);
+  await browser.study.logger.debug(["dataReceiver - DEBUG", logEntry]);
   await telemetrySender.submitTelemetryPayload("openwpmLog", logEntry);
 };
 
 export const logWarn = async function(msg) {
   const level = "warn";
   const logEntry = { level, msg };
-  await browser.study.logger.warn(["dataReceiver", logEntry]);
+  await browser.study.logger.warn(["dataReceiver - WARN", logEntry]);
   await telemetrySender.submitTelemetryPayload("openwpmLog", logEntry);
 };
 
 export const logError = async function(msg) {
   const level = "error";
   const logEntry = { level, msg };
-  await browser.study.logger.error(["dataReceiver", logEntry]);
+  await browser.study.logger.error(["dataReceiver - ERROR", logEntry]);
   await telemetrySender.submitTelemetryPayload("openwpmLog", logEntry);
 };
 
 export const logCritical = async function(msg) {
   const level = "error";
   const logEntry = { level, msg };
-  await browser.study.logger.error(["dataReceiver", logEntry]);
+  await browser.study.logger.error(["dataReceiver - CRITICAL", logEntry]);
   await telemetrySender.submitTelemetryPayload("openwpmLog", logEntry);
 };
 
 export const saveRecord = async function(instrument, record) {
-  await browser.study.logger.error([
+  await browser.study.logger.log([
     "dataReceiver - saveRecord - instrument, record",
     instrument,
-    record,
+    // record, // Too large to send to console
   ]);
   await telemetrySender.submitTelemetryPayload(instrument, record);
 };
 
 export const saveContent = async function(content, contentHash) {
-  await browser.study.logger.error([
+  await browser.study.logger.log([
     "dataReceiver - saveContent - contentHash, content.length",
     contentHash,
     content.length,
