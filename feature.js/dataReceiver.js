2b9d1889e611a8669d792a3e917e27ce760c043f
Fredrik Wollsén <fredrik@neam.se>
2018-10-28 03:14:59 +0300

    Data receiver logs using browser.study.logger

--- feature.js/dataReceiver.js
+++ feature.js/dataReceiver.js
@@ -1,73 +1,46 @@
 import { storeInDb } from "./db";
 
-// send to console if debugging
-const debugging = true;
-
 export const logInfo = async function(msg) {
   const level = "info";
   const logEntry = { level, msg };
-  if (debugging) {
-    console.log(logEntry);
-  }
+  await browser.study.logger.log(["dataReceiver - INFO: ", logEntry]);
   await storeInDb("openwpmLog", logEntry);
 };
 
 export const logDebug = async function(msg) {
   const level = "debug";
   const logEntry = { level, msg };
-  if (debugging) {
-    console.log(logEntry);
-  }
+  await browser.study.logger.debug(["dataReceiver", logEntry]);
   await storeInDb("openwpmLog", logEntry);
 };
 
 export const logWarn = async function(msg) {
   const level = "warn";
   const logEntry = { level, msg };
-  if (debugging) {
-    console.warn(logEntry);
-  }
+  await browser.study.logger.warn(["dataReceiver", logEntry]);
   await storeInDb("openwpmLog", logEntry);
 };
 
 export const logError = async function(msg) {
   const level = "error";
   const logEntry = { level, msg };
-  if (debugging) {
-    console.error(logEntry);
-  }
+  await browser.study.logger.error(["dataReceiver", logEntry]);
   await storeInDb("openwpmLog", logEntry);
 };
 
 export const logCritical = async function(msg) {
   const level = "error";
   const logEntry = { level, msg };
-  if (debugging) {
-    console.error(logEntry);
-  }
+  await browser.study.logger.error(["dataReceiver", logEntry]);
   await storeInDb("openwpmLog", logEntry);
 };
 
 export const saveRecord = async function(instrument, record) {
-  if (debugging) {
-    console.log(
-      "OpenWPM dataReceiver saveRecord",
-      instrument,
-      JSON.stringify(record),
-    );
-    return;
-  }
+  await browser.study.logger.error(["dataReceiver - saveRecord - instrument, record", instrument, record]);
   await storeInDb(instrument, record);
 };
 
 export const saveContent = async function(content, contentHash) {
-  if (debugging) {
-    console.log(
-      "OpenWPM dataReceiver saveRecord - contentHash, content.length:",
-      contentHash,
-      content.length,
-    );
-    return;
-  }
+  await browser.study.logger.error(["dataReceiver - saveContent - contentHash, content.length", contentHash, content.length]);
   await storeInDb("savedContent", { content, contentHash });
 };
