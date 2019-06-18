cca7f545d88167482ccc9357fef05df609c20d61
Fredrik Wollsén <fredrik@neam.se>
2019-06-19 00:21:32 +0300

    Avoid browser.tabs.query browser console error message

--- feature.js/ActiveTabDwellTimeMonitor.ts
+++ feature.js/ActiveTabDwellTimeMonitor.ts
@@ -10,12 +10,18 @@ export class ActiveTabDwellTimeMonitor {
     const intervalMs = 250;
 
     this.interval = setInterval(async () => {
-      // const tab = await browser.tabs.getCurrent();
-      const activeTabs = await browser.tabs.query({
+      // First check non-private tabs without specifying currentWindow: true to avoid bug https://bugzilla.mozilla.org/show_bug.cgi?id=1560025
+      const activeNonPrivateTabs = await browser.tabs.query({
+        active: true,
+      });
+      if (activeNonPrivateTabs.length === 0) {
+        return;
+      }
+      const activeNonPrivateTabsInTheCurrentWindow = await browser.tabs.query({
         currentWindow: true,
         active: true,
       });
-      activeTabs.map(tab => {
+      activeNonPrivateTabsInTheCurrentWindow.map(tab => {
         if (this.tabActiveDwellTimes[tab.id] === undefined) {
           this.tabActiveDwellTimes[tab.id] = intervalMs;
         } else {
